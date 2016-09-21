5da1913e52e9d3af8d62da097a8d0965316df812
Dmitriy Burlutskiy <vidokx@gmail.com>
2016-09-21 13:38:34 +0300

    add byebug

--- lib/config/sources/yaml_source.rb
+++ lib/config/sources/yaml_source.rb
@@ -18,7 +18,7 @@ module Config
         end
 
         return {} unless result
-
+        byebug
         if @section_path
           # result = { File.basename(@path.to_s, '.*') => result }
       	  # dir = File.dirname(@path).split('/')
